fcd2561ee28d311b10db8c6bbd221ebf5ef68dd7
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2022-09-19 12:52:37 -0600

    Update install_course_packages.R

--- scripts/install_course_packages.R
+++ scripts/install_course_packages.R
@@ -1,15 +1,20 @@
 # Use bioconductor to install packages
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install(c('phyloseq','metagenomeSeq'))
+BiocManager::install(c('phyloseq','metagenomeSeq','DESeq2','genefilter'))
 
 # Install packages from CRAN repository
-install.packages(c("dplyr","broom","data.table","tidyr","forcats", "vegan","plyr", "devtools",
-                   "pheatmap","dendsort","ggrepel","ggpubr","Rtools","tidyverse","ggthemes"))
+install.packages(c("ggplot2","broom","data.table","forcats", "vegan","devtools",
+                   "pheatmap","dendsort","ggrepel","ggpubr","Rtools","tidyverse","ggthemes","GUniFrac","stringr",
+                   "scales","knitr","readr","randomcoloR", "kableExtra", "ggdendro"))
+
+#install.packages("devtools")
+devtools::install_github("hadley/devtools")
 
 # Install "pairwiseAdonis" from github for adding pairwise comparisons to models build with the adonis() function
-install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
-library(pairwiseAdonis)
+devtools::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+devtools::install_github('twbattaglia/btools') # requires DESeq2 and genefilter
+devtools::install_github("vmikk/metagMisc")
 
 # Optional package used for comparing the results from multiple types of differential abundance tests
 # install_github("Russel88/DAtest")
